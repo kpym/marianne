72e7bd2dbac074b77a431d8cd1158517506645b7
kpym <kpym.tzanev@gmail.com>
2020-10-27 18:21:59 +0100

    simplification de la traduction de l'aide

--- translate.go
+++ translate.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"strings"
 )
@@ -10,19 +9,19 @@ type trad struct {
 	from, to string
 }
 
-var traductions = []trad{
-	{" (default [])", ""},
-	{"default", "par défaut"},
-	{" strings ", "         "},
-	{" string ", "        "},
-	{" uints ", "       "},
-	{" int ", "     "},
-	{"bad flag syntax:", "mauvaise syntaxe du paramètre :"},
-	{"unknown flag:", "paramètre inconnu :"},
-	{"unknown shorthand flag:", "paramètre court inconnu :"},
-	{"flag needs an argument:", "Le paramètre nécessite un argument :"},
-	{" in ", " dans "},
-}
+var traductions = strings.NewReplacer(
+	" (default [])", "",
+	"default", "par défaut",
+	" strings ", "         ",
+	" string ", "        ",
+	" uints ", "       ",
+	" int ", "     ",
+	"bad flag syntax:", "mauvaise syntaxe du paramètre :",
+	"unknown flag:", "paramètre inconnu :",
+	"unknown shorthand flag:", "paramètre court inconnu :",
+	"flag needs an argument:", "paramètre sans argument :",
+	" in ", " dans ",
+)
 
 // FrenchTranslator est un io.Writer qui traduit quelques phrases d'angalsi en français et
 // renvoie le résultat au w
@@ -32,12 +31,5 @@ type FrenchTranslator struct {
 
 // Write traduit le message p et renvoie la traduction à fw.w
 func (fw FrenchTranslator) Write(p []byte) (n int, err error) {
-	out := string(p)
-
-	for _, tr := range traductions {
-		out = strings.ReplaceAll(out, tr.from, tr.to)
-	}
-
-	fmt.Fprintf(fw.w, out)
-	return len(p), nil
+	return traductions.WriteString(fw.w, string(p))
 }
